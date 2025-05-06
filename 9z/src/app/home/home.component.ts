433afe0abece912740d1a87f67f8c912ae1968e0
santiherzfeld <santiherzfeld@gmail.com>
2025-05-06 17:16:20 -0300

    add 9z pro banners

--- 9z/src/app/home/home.component.ts
+++ 9z/src/app/home/home.component.ts
@@ -8,7 +8,6 @@ import { Router } from '@angular/router';
 SwiperCore.use([Scrollbar, EffectFade]);
 declare var particlesJS: any;
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -28,6 +27,7 @@ export class HomeComponent implements OnInit {
       delay: 5000,
     },
     watchSlidesProgress: true,
+    loop: true,
   };
 
   title: string = '';
@@ -35,7 +35,7 @@ export class HomeComponent implements OnInit {
   buttonCta: any = {
     title: '',
     localRedirect: null,
-    redirect: ''
+    redirect: '',
   };
 
   sponsors: any[] = [];
@@ -43,7 +43,8 @@ export class HomeComponent implements OnInit {
   category2: string = '';
   category3: string = '';
   heroImage: string = '';
-  
+  heroSlides: Array<{ image: string; url: string; title: string }> = [];
+
   heroSlide: string = '';
   tweetList: Array<any> = [];
   twitchStreamList: Array<any> = [];
@@ -96,7 +97,7 @@ export class HomeComponent implements OnInit {
     await this.getSponsors();
     await this.getPageNews();
     await this.getVideoGalleries();
-    
+
     this.socialServices.updateMetaTags('Home');
 
     let json = JSON.parse(localStorage.getItem('matches')!);
@@ -127,7 +128,6 @@ export class HomeComponent implements OnInit {
       this.getMatches(json);
     }
 
-    
     let result = JSON.parse(localStorage.getItem('streamers')!);
     if (result === 'null' || result === null) {
       setTimeout(async () => {
@@ -137,8 +137,8 @@ export class HomeComponent implements OnInit {
         this.twitchStreamList = [];
         await this.getStreams(this.streamersList);
         this.streamersList.forEach(async (streamer: any) => {
-          await this.getStreamsByUserId(streamer.user_id)
-        })
+          await this.getStreamsByUserId(streamer.user_id);
+        });
         // for (let i = 0; i < this.streamersList.length; i++) {
         //   await this.getStreamsByUserId(this.streamersList[i].user_id);
         // }
@@ -150,11 +150,10 @@ export class HomeComponent implements OnInit {
       await this.getStreams(this.streamersList);
 
       this.streamersList.forEach(async (streamer: any) => {
-        await this.getStreamsByUserId(streamer.user_id)
-      })
+        await this.getStreamsByUserId(streamer.user_id);
+      });
       this.commonServices.sendStreamsDataUpdate(this.twitchStreamList);
     }
-
   }
 
   getShownCategories(jsonShownCategories: any) {
@@ -167,16 +166,16 @@ export class HomeComponent implements OnInit {
     matches.result.forEach((match: any) => {
       if (match.game.name.toLowerCase() === 'fortnite') {
         if (!match.position && !match.teamMember) {
-          this.upcomingMatchList.push(match)
+          this.upcomingMatchList.push(match);
         } else {
-          this.pastMatchList.push(match)
+          this.pastMatchList.push(match);
         }
-      } else if(match.result1 === '0' && match.result2 === '0') {
-        this.upcomingMatchList.push(match)
+      } else if (match.result1 === '0' && match.result2 === '0') {
+        this.upcomingMatchList.push(match);
       } else {
-        this.pastMatchList.push(match)
+        this.pastMatchList.push(match);
       }
-    })
+    });
     // for (let i = 0; i < matches.result.length; i++) {
     //   if (matches.result[i].game.name.toLocaleLowerCase() === 'fortnite') {
     //     if (
@@ -220,15 +219,16 @@ export class HomeComponent implements OnInit {
     const y = event.clientY - rect.top;
     this.style = {
       '--x': `${x}px`,
-      '--y': `${y}px`
+      '--y': `${y}px`,
     };
   }
 
   async getSponsors() {
-    const response: any = await this.sanityServices.getSponsorsByPage('Inicio')
-    .catch(error => {
-      console.log(error);
-    });
+    const response: any = await this.sanityServices
+      .getSponsorsByPage('Inicio')
+      .catch((error) => {
+        console.log(error);
+      });
 
     if (response?.logos) {
       this.sponsors = response.logos;
@@ -236,40 +236,70 @@ export class HomeComponent implements OnInit {
   }
 
   async getHomeData() {
-    const response: any = await this.sanityServices.getHome()
-    .catch(error => {
+    const response: any = await this.sanityServices.getHome().catch((error) => {
       console.log(error);
-    })
+    });
 
     if (response) {
       this.title = response.title;
       this.buttonCta.title = response.titleCTA;
       this.buttonCta.localRedirect = response.localRedirect;
       this.buttonCta.redirect = response.urlCTA;
-      this.heroSlide = window.innerWidth < 767 ? response.imageHeroMob : response.imageHero
+      this.heroSlide =
+        window.innerWidth < 767 ? response.imageHeroMob : response.imageHero;
+
+      this.heroSlides = [
+        {
+          image: '/assets/home/1.jpg',
+          url: 'https://shop.9z.gg/shop/category/9z-pro-448',
+          title: '9z Pro',
+        },
+        {
+          image: '/assets/home/2.jpg',
+          url: 'https://shop.9z.gg/shop/wgm2308-p-mouse-9z-spectre-violeta-262868?category=448#attr=',
+          title: '9z Spectre',
+        },
+        {
+          image: '/assets/home/3.jpg',
+          url: 'https://shop.9z.gg/shop/wlktkl2405-teclado-inalambrico-9z-ghoul-blanco-262866?category=448#attr=',
+          title: '9z Ghoul',
+        },
+        {
+          image: '/assets/home/4.jpg',
+          url: 'https://shop.9z.gg/shop/mp-cu-45404-mousepad-9z-phantom-chico-262872?category=448#attr=',
+          title: '9z Phantom Chico',
+        },
+        {
+          image: '/assets/home/5.jpg',
+          url: 'https://shop.9z.gg/shop/mp-cu-90434-mousepad-9z-phantom-grande-262873?category=448#attr=',
+          title: '9z Phantom Grande',
+        },
+      ];
     }
   }
 
   async getPageNews() {
-    const response: any = await this.sanityServices.getPageNews()
-    .catch(error => {
-      console.log(error);
-    });
+    const response: any = await this.sanityServices
+      .getPageNews()
+      .catch((error) => {
+        console.log(error);
+      });
 
     if (response) {
       this.featuredNewsList = response.featuredNews;
-      this.commonServices.sendfeaturedNewsListClubUpdate(
-        this.featuredNewsList
-      );
+      this.commonServices.sendfeaturedNewsListClubUpdate(this.featuredNewsList);
     }
-    this.featuredNewsList.forEach((post: any) => post.slug = this.getName(post.title))
+    this.featuredNewsList.forEach(
+      (post: any) => (post.slug = this.getName(post.title))
+    );
   }
 
   async getVideoGalleries() {
-    const response: any = await this.sanityServices.getVideoGalleries()
-    .catch(error => {
-      console.log(error);
-    });
+    const response: any = await this.sanityServices
+      .getVideoGalleries()
+      .catch((error) => {
+        console.log(error);
+      });
 
     if (response) {
       this.videoGalleries = response;
@@ -277,20 +307,28 @@ export class HomeComponent implements OnInit {
   }
 
   ctaRedirect(url: string) {
-    this.router.navigate([url]);
+    if (url) {
+      this.router.navigate([url]);
+    }
   }
 
   async getTwitchUserId(list: Array<any>) {
-    const response: any = await this.socialServices.getTwitchUserId(list)
-    .catch(async err => {
-      console.log('Se ha producido un error al intentar obtener los User_id de twitch.');
-      if (err !== undefined) {
-        console.log(err);
-        if (err.error.error === 'Unauthorized' && err.error.message === 'Invalid OAuth token') {
-          await this.refreshTwitchToken();
+    const response: any = await this.socialServices
+      .getTwitchUserId(list)
+      .catch(async (err) => {
+        console.log(
+          'Se ha producido un error al intentar obtener los User_id de twitch.'
+        );
+        if (err !== undefined) {
+          console.log(err);
+          if (
+            err.error.error === 'Unauthorized' &&
+            err.error.message === 'Invalid OAuth token'
+          ) {
+            await this.refreshTwitchToken();
+          }
         }
-      }
-    });
+      });
     if (response !== null && response !== undefined) {
       const userId = response.data[0].id;
       await this.getStreamsByUserId(userId);
@@ -298,75 +336,101 @@ export class HomeComponent implements OnInit {
   }
 
   async getStreams(streamersList: Array<any>) {
-    const twitchStreams = await this.socialServices.getStreams(streamersList)
+    const twitchStreams = await this.socialServices
+      .getStreams(streamersList)
       .catch(async (err) => {
-        console.log('Se ha producido un error al intentar obtener los streams.');
+        console.log(
+          'Se ha producido un error al intentar obtener los streams.'
+        );
         if (err !== undefined) {
           console.log(err);
-          if (err.error.error === 'Unauthorized' && err.error.message === 'Invalid OAuth token') {
+          if (
+            err.error.error === 'Unauthorized' &&
+            err.error.message === 'Invalid OAuth token'
+          ) {
             await this.refreshTwitchToken();
           }
         }
       });
-      if (twitchStreams) {
-        twitchStreams.data.forEach((stream: any) => {
-          stream.thumbnail_url = stream.thumbnail_url.replace('{width}', '1280'),
-          stream.thumbnail_url = stream.thumbnail_url.replace('{height}', '720');
-          streamersList.forEach((streamer: any) => {
-            if (stream.user_id === streamer.user_id) {
-              stream.user_image = streamer.user_image
-            }
-          })
-        })
+    if (twitchStreams) {
+      twitchStreams.data.forEach((stream: any) => {
+        (stream.thumbnail_url = stream.thumbnail_url.replace(
+          '{width}',
+          '1280'
+        )),
+          (stream.thumbnail_url = stream.thumbnail_url.replace(
+            '{height}',
+            '720'
+          ));
+        streamersList.forEach((streamer: any) => {
+          if (stream.user_id === streamer.user_id) {
+            stream.user_image = streamer.user_image;
+          }
+        });
+      });
       this.twitchStreamList = twitchStreams.data;
     }
   }
 
   async getStreamsByUserId(userId: string) {
-    const twitchStreams = await this.socialServices.getStreamsByUserId(userId)
-    .catch(async (err) => {
-      console.log('Se ha producido un error al intentar obtener los streams.');
-      if (err !== undefined) {
-        console.log(err);
-        if (err.error.error === 'Unauthorized' && err.error.message === 'Invalid OAuth token') {
-          await this.refreshTwitchToken();
+    const twitchStreams = await this.socialServices
+      .getStreamsByUserId(userId)
+      .catch(async (err) => {
+        console.log(
+          'Se ha producido un error al intentar obtener los streams.'
+        );
+        if (err !== undefined) {
+          console.log(err);
+          if (
+            err.error.error === 'Unauthorized' &&
+            err.error.message === 'Invalid OAuth token'
+          ) {
+            await this.refreshTwitchToken();
+          }
         }
-      }
-    });
+      });
     if (twitchStreams) {
       twitchStreams.data.forEach((stream: any) => {
-        stream.thumbnail_url = stream.thumbnail_url.replace('%{width}', '1280'),
-        stream.thumbnail_url = stream.thumbnail_url.replace('%{height}', '720');
+        (stream.thumbnail_url = stream.thumbnail_url.replace(
+          '%{width}',
+          '1280'
+        )),
+          (stream.thumbnail_url = stream.thumbnail_url.replace(
+            '%{height}',
+            '720'
+          ));
         this.streamersList.forEach((streamer: any) => {
           if (stream.user_id === streamer.user_id) {
-            stream.user_image = streamer.user_image
+            stream.user_image = streamer.user_image;
           }
-        })
-      })
+        });
+      });
       this.twitchStreamList = this.twitchStreamList.concat(twitchStreams.data);
     }
   }
 
   async refreshTwitchToken() {
-    const response: any = await this.socialServices.refreshTwitchToken()
-    .catch(err => {
-      console.log(err);
-    });
+    const response: any = await this.socialServices
+      .refreshTwitchToken()
+      .catch((err) => {
+        console.log(err);
+      });
 
     if (response !== undefined) {
       console.log('Se refresheó twitch access token');
     }
-   }
+  }
 
-   getName(name: string) {
-    name = name.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  getName(name: string) {
+    name = name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
     // name = name.replace(/&nbsp;|\u00a0/gi,"")
-    return name.toLowerCase()
-    .trim()
-    .replace(/[^a-z0-9-]/g, '-')
-    .replace(/-+/g, '-')
-    .replace(/^-|-$/g, '')
-    .replace('"', '')
-    .replace(':', '-' );
+    return name
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9-]/g, '-')
+      .replace(/-+/g, '-')
+      .replace(/^-|-$/g, '')
+      .replace('"', '')
+      .replace(':', '-');
   }
 }
